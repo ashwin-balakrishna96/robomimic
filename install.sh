f2a30aca2d160ee8abc513824b42a5ffdeb43a2c
Ashwin Balakrishna <ashwin.balakrishna@tri.global>
2023-08-24 16:20:09 -0700

    fix install script

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
-;
+# Mujoco Setup
+wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz -P ~/.mujoco/
+tar -xf ~/.mujoco/mujoco210-linux-x86_64.tar.gz 
+rm ~/.mujoco/mujoco210-linux-x86_64.tar.gz
+
+# System wide dependencies
+sudo apt-get install screen
+sudo apt-get install libghc-x11-dev
+sudo apt-get install libglew-dev
+sudo apt-get install patchelf
+sudo apt-get install ffmpeg
+
+# Add to Bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.mujoco/mujoco210/bin" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia" >> ~/.bashrc
+
+# Python dependencies
+pip install -r robomimic_requirements.txt
